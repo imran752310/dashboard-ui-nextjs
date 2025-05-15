7d4514481d370ab718cd2d6975b3a3fc0ead15fd
imran752310 <muhammadimran752310@gmail.com>
2025-05-15 23:26:51 +0500

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,16 @@ export default function Home() {
   return (
    <>
    <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+     <DashboardCard 
+      title ="Categories"
+      count={100}
+      icon={<Folder className="text-slate-500" size={72} />}
+      />
       <DashboardCard 
       title ="Posts"
       count={100}
       icon={<Newspaper className="text-slate-500" size={72} />}
       />
-
-      <DashboardCard 
-      title ="Categories"
-      count={100}
-      icon={<Folder className="text-slate-500" size={72} />}
-      />
-
        <DashboardCard 
       title ="Users"
       count={1200}
