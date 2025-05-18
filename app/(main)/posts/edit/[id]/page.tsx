3e497311963ff1273569f6df9f8e7d4f5cf28b7e
imran752310 <muhammadimran752310@gmail.com>
2025-05-18 23:59:13 +0500

    ok

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -11,6 +11,7 @@ import {
   FormMessage
 } from '@/components/ui/form';
 import posts from '@/data/posts';
+import { Input } from '@/components/ui/input';
 
 
 const formSchema = z.object({
@@ -38,7 +39,7 @@ const PostEditPage = ({params}: PostEditPageProps) => {
   const post = posts.find((post)=> post.id === params.id);
   
   const form = useForm<z.infer<typeof formSchema>>({
-   resolver: zodResolver(formSchema)
+   resolver: zodResolver(formSchema),
     defaultValues: {
       title: post?.title || '',
       body: post?.body || '',
@@ -47,13 +48,30 @@ const PostEditPage = ({params}: PostEditPageProps) => {
     },
   });
 
+  const handleSubmit = (data: z.infer<typeof formSchema>)=>{
+    console.log(data)
+  }
+
   return (
     <div>
       <Backbutton text='Back To Post' link={'/posts'} />
       <h3 className='text-2xl mb-4'>Edit Post</h3>
       <Form {...form}>
-        <form>
-          
+        <form onSubmit={ form.handleSubmit(handleSubmit)}>
+     <FormField control={form.control}
+     name='title'
+     render={({ field }) =>(
+       <FormItem>
+      <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-secondray/70'>Title</FormLabel>
+      <FormControl>
+        <Input className="bg-slate-100 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" placeholder="Enter Title" {...field} />
+      </FormControl>
+     
+      <FormMessage />
+    </FormItem>
+     )}
+     />
+
         </form>
 
       </Form>
